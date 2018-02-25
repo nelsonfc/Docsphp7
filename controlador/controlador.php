2755e5927167ec009d988f5c4a4d016a83ad2277
nelsonfc <nfiguerc@gmail.com>
2018-02-25 19:02:56 -0300

    varialbes y validacion parte 1
    Agregamos las varibles  de los arreglos para devolver ademas de un contador de errores para saber cuando vienen datos vacios.

--- controlador/controlador.php
+++ controlador/controlador.php
@@ -5,7 +5,32 @@
  * Date: 25/02/2018
  * Time: 17:42
  */
-
+//recepcionamos los datos
 $datos = $_POST['datos'];
+//variables para manejar errores
+$contadorErrores =0;
+//Arreglos para devolver en json
+$fechasoriginales = [];
+$arreglofechas =[];
+$arreglodias = [];
 
-echo json_encode($datos);
+
+for($i=0; $i < count($datos); $i +=2){
+
+    $fecha = $datos[$i]['value'];
+    $numero = $datos[$i+1]['value'];
+
+    //comprovamos que los campos no vengan nulos
+    if($fecha === '' || $numero === ''){
+        $contadorErrores =1;
+        break;
+    }
+
+}
+
+if($contadorErrores == 0){
+
+}else {
+
+    echo json_encode("error");
+}
