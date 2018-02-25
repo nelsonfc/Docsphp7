d7661c28431ef0eae576b7c5e479e2a058fc9f5e
nelsonfc <nfiguerc@gmail.com>
2018-02-25 18:06:29 -0300

    Estructura
    Se define la estructura de la pagina

--- vista/index.php
+++ vista/index.php
@@ -5,3 +5,55 @@
  * Date: 25/02/2018
  * Time: 17:40
  */
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Formulario - Tionvel</title>
+    <meta charset="UTF-8">
+    <!-- importamos bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- importamos la hoja de estilos -->
+    <link REL=stylesheet HREF="../estilo/estilo.css" TYPE="text/css" MEDIA=screen>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="col-md-12">
+    <header>
+        <h1>Tionvel</h1>
+    </header>
+    <!-- este section se utilizara para mostrar el formulario -->
+    <section>
+        <div class="panel panel-primary">
+            <div class="panel-heading"><b style="font-size: 18px">Formulario</b></div>
+            <div class="panel-body">
+
+
+            </div>
+        </div>
+
+    </section>
+
+    <!-- este section se utilizara para mostrar los resultados -->
+    <section>
+        <div id="panel" class="panel panel-success">
+            <div class="panel-heading">Resultados</div>
+            <div class="panel-body">
+
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        Autor: Nelson Figueroa Febrero 2018
+    </footer>
+</div>
+</body>
+
+</html>
+
